c0f0a8f06ab542b43f1e9e83c55cc139c9d16b55
kungfooman <lama12345@gmail.com>
2019-11-30 22:06:30 +0100

    npm install @material-ui/pickers for calendars

--- src/components/PageMain.tsx
+++ src/components/PageMain.tsx
@@ -6,6 +6,7 @@ import PageMen from "./PageMen";
 import PageAccessories from "./PageAccessories";
 import TestParallaxSlide from "./TestParallaxSlide";
 import { TestSwipableViews } from "./TestSwipeableViews";
+import SwipeableViews from "./ReactSwipeableViews";
 
 export default class PageMain extends React.Component {
 	constructor(props) {
@@ -19,11 +20,20 @@ export default class PageMain extends React.Component {
 				<img src="images/carousel_2.png"></img>
 			</Carousel>
 
-			{/*<TestParallaxSlide style="width: 40%"></TestParallaxSlide>*/}
-			<TestSwipableViews className="Carousel">
+			{/*
+			<TestParallaxSlide style="width: 40%"></TestParallaxSlide>
+			*/}
+
+			{/*
+			<SwipeableViews enableMouseEvents hysteresis={0.2} slideClassName="CarouselItem">
 				<img src="images/carousel_1.png"></img>
 				<img src="images/carousel_2.png"></img>
-			</TestSwipableViews>
+			</SwipeableViews>
+			*/}
+		
+			{/*
+			<TestSwipableViews/>
+			*/}
 
 			<div className="frontPageLink">
 				<a onClick={setAppComponent.bind(this, <PageWomen/>)}>
